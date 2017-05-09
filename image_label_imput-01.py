a7c999a9736b60602ea50676a3a8b804e6c6f20b
zechocapic <zechocapic@gmail.com>
2017-05-09 22:22:53 +0200

    working queue with basic features and labels

--- image_label_imput-01.py
+++ image_label_imput-01.py
@@ -1,33 +1,32 @@
 import tensorflow as tf
 
 # create data features and labels
-listFileNames = ['renault/download.jpg', 'renault/download1.jpg', 'renault/download2.jpg']
-listLabels = ['car0', 'car1', 'car2']
+listFileNames = ['renault/download00.jpg', 'renault/download01.jpg', 'renault/download02.jpg',
+                 'renault/download03.jpg', 'renault/download04.jpg', 'renault/download05.jpg',
+                 'renault/download06.jpg', 'renault/download07.jpg', 'renault/download08.jpg',
+                 'renault/download09.jpg', 'renault/download10.jpg', 'renault/download11.jpg',
+                 'renault/download12.jpg', 'renault/download13.jpg']
 
-inputQueue = tf.train.slice_input_producer([listFileNames, listLabels], shuffle=False, seed=False)
+inputQueue = tf.train.string_input_producer(listFileNames, shuffle=True, seed=True)
 
-content = tf.read_file(inputQueue[0])
+reader = tf.WholeFileReader()
+key, content = reader.read(inputQueue)
 image = tf.image.decode_jpeg(content, channels=1)
 resizedImage = tf.image.resize_images(image, [4, 4])
+lab = key
 
-trainFeature = resizedImage
-trainLabel = inputQueue[1]
-
-trainFeatures = tf.train.batch([trainFeature], 2)
-trainLabels = tf.train.batch([trainLabel], 2)
-
-# tensorflow features and labels
-features = tf.placeholder(tf.float32, [3, 16])
-labels = tf.placeholder(tf.float32, [3, 1])
+# trainFeatures = tf.train.batch([trainFeature], 3)
+# trainLabels = tf.train.batch([key], 3)
 
 with tf.Session() as sess:
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
-    f, l = sess.run([trainFeatures, trainLabels])
-    print(f)
-    print(l)
-    print(trainFeature)
-    print(trainFeatures)
+    for i in range(50):
+        feature, label = sess.run([resizedImage, lab])
+        print("Feature")
+        print(feature)
+        print("Label")
+        print(label)
 
     coord.request_stop()
     coord.join(threads=threads)
