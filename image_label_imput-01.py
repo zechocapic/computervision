5751b535ed9a067d29270f6fc2a413d660bc6492
zechocapic <zechocapic@gmail.com>
2017-05-09 22:32:07 +0200

    renaming some variables

--- image_label_imput-01.py
+++ image_label_imput-01.py
@@ -1,18 +1,18 @@
 import tensorflow as tf
 
 # create data features and labels
-listFileNames = ['renault/download00.jpg', 'renault/download01.jpg', 'renault/download02.jpg',
+list_filename = ['renault/download00.jpg', 'renault/download01.jpg', 'renault/download02.jpg',
                  'renault/download03.jpg', 'renault/download04.jpg', 'renault/download05.jpg',
                  'renault/download06.jpg', 'renault/download07.jpg', 'renault/download08.jpg',
                  'renault/download09.jpg', 'renault/download10.jpg', 'renault/download11.jpg',
                  'renault/download12.jpg', 'renault/download13.jpg']
 
-inputQueue = tf.train.string_input_producer(listFileNames, shuffle=True, seed=True)
+input_queue = tf.train.string_input_producer(list_filename, shuffle=True, seed=True)
 
 reader = tf.WholeFileReader()
-key, content = reader.read(inputQueue)
+key, content = reader.read(input_queue)
 image = tf.image.decode_jpeg(content, channels=1)
-resizedImage = tf.image.resize_images(image, [4, 4])
+resized_image = tf.image.resize_images(image, [4, 4])
 lab = key
 
 # trainFeatures = tf.train.batch([trainFeature], 3)
@@ -22,7 +22,7 @@ with tf.Session() as sess:
     coord = tf.train.Coordinator()
     threads = tf.train.start_queue_runners(coord=coord)
     for i in range(50):
-        feature, label = sess.run([resizedImage, lab])
+        feature, label = sess.run([resized_image, lab])
         print("Feature")
         print(feature)
         print("Label")
